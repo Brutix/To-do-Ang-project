e49d27774fc83fa67e3453cf220e76d2b7b9cbc5
Selena <brutix80@gmail.com>
2022-08-09 23:42:48 +0300

    Pavel

--- Angular-project/src/app/app.module.ts
+++ Angular-project/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ProductComponent} from "./components/product/product.component";
+import {Product} from "./models/product"
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+
   ],
   imports: [
     BrowserModule
