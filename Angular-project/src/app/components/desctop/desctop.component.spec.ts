ca4c01037b4a706393d116e8dbf319a33e77258d
Brutix <53449943+Brutix@users.noreply.github.com>
2022-08-11 16:15:36 +0300

    Routes and Guards

---
+++ Angular-project/src/app/components/desctop/desctop.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DesctopComponent } from './desctop.component';
+
+describe('DesctopComponent', () => {
+  let component: DesctopComponent;
+  let fixture: ComponentFixture<DesctopComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DesctopComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DesctopComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Angular-project/src/app/components/desctop/desctop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desctop',
+  templateUrl: './desctop.component.html',
+  styleUrls: ['./desctop.component.css']
+})
+export class DesctopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
