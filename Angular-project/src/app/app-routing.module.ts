eaf29f5f4f10d785552fa904da83fa7e9acca938
Pavel Kharlamov <brutix80@gmail.com>
2022-08-18 02:51:39 +0300

    add nav and some html/css

--- Angular-project/src/app/app-routing.module.ts
+++ Angular-project/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProductComponent} from './components/product/product.component';
 import {HomepageComponent} from './components/homepage/homepage.component';
 import {ProductGuard} from './product.guard';
+import {BusketComponent} from "./components/busket/busket.component";
 
 
 const routes: Routes = [
 
   { path: 'product', component: ProductComponent, canActivate: [ProductGuard] },
   { path: 'home', component: HomepageComponent },
+  { path: 'busket', component: BusketComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
